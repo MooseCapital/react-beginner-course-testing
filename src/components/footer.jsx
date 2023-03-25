a6da4a954f5b145cf47998273bb16c92635ed0c6
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-03-25 14:33:22 -0500

    project complete, business card

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,6 +1,11 @@
 function Footer() {
     return (
-        <footer></footer>
+        <footer>
+            <img src="src/assets/Twitter Icon.png" alt="" className="twitter"/>
+            <img src="src/assets/Facebook Icon.png" alt="" className="facebook"/>
+            <img src="src/assets/Instagram Icon.png" alt="" className="instagram"/>
+            <img src="src/assets/GitHub Icon.png" alt="" className="github"/>
+        </footer>
     )
 }
 export default Footer

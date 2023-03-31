dd22a927f6eca0303cb1f0a7378339d38e22dd1e
Moose Capital <48196495+MooseCapital@users.noreply.github.com>
2023-03-31 13:04:35 -0500

    travel journal project data save

---
+++ src/data/travel-data.jsx
@@ -0,0 +1,33 @@
+export default [
+    {
+        img: "assets-traveljournal/norway.jpg",
+        title: "Kvalvika beach",
+        location: "Norway",
+        maps_link: "https://www.google.com/maps/place/Kvalvika+Beach/@68.0763759,13.0367089,13091m/data=!3m1!1e3!4m6!3m5!1s0x45de19c203387e97:0xa98bcbe8be9f0dc4!8m2!3d68.0774028!4d13.0982556!16s%2Fg%2F11c1wwt1tv?hl=en",
+        start_date: "14 Jan, 2019",
+        end_date: "21 Jan, 2019",
+        description: "Kvalvika Beach, is a famous beach located on the north western side of lofoten, facing the mighty atlantic ocean and only accessible by foot. The drive to get there is a treat by itself, and the hike to the beach is relatively easy",
+        id: "1",
+    },
+    {
+        img: "assets-traveljournal/grand-tetons.JPG",
+        title: "Grand Teton",
+        location: "United States",
+        maps_link: "https://www.google.com/maps/search/grand+teton+national+park/@43.6843412,-110.8714381,42666m/data=!3m1!1e3?hl=en",
+        start_date: "9 May, 2021",
+        end_date: "18 May, 2021",
+        description: "The Teton Range is a mountain range of the Rocky Mountains in North America. It extends for approximately 40 miles (64 km) in a north–south direction through the U.S. state of Wyoming, east of the Idaho state line.",
+        id: "2",
+    },
+    {
+        img: "assets-traveljournal/bermuda.jpg",
+        title: "Warwick Long bay",
+        location: "Bermuda",
+        maps_link: "https://www.google.com/maps/place/Warwick+Long+Bay/@32.256696,-64.8173102,1631m/data=!3m1!1e3!4m10!1m2!2m1!1sbermuda+pink+sand+beach!3m6!1s0x8a2d15d80d5a27b1:0xd431a94fe0c98f73!8m2!3d32.2573939!4d-64.8103667!15sChdiZXJtdWRhIHBpbmsgc2FuZCBiZWFjaFoZIhdiZXJtdWRhIHBpbmsgc2FuZCBiZWFjaJIBBWJlYWNomgEkQ2hkRFNVaE5NRzluUzBWSlEwRm5TVVIxYVUxUWFqSjNSUkFC4AEA!16s%2Fg%2F11c70bh97m",
+        start_date: "20 July, 2021",
+        end_date: "27 July, 2021",
+        description: "Warwick Long bay is a magnificent half mile stretch of pink sands. Against a backdrop of grape and cedar trees and low grasses, It's one of the most picturesque beaches of Bermuda",
+        id: "3",
+    },
+
+]
